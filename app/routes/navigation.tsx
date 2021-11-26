0c1bb7fe54a458df46e7f7aeb376732a2d48fa14
codePradeep <pradeep.sharma1@successive.tech>
2021-11-26 18:28:08 +0530

    Integrated Google maps

--- app/routes/navigation.tsx
+++ app/routes/navigation.tsx
@@ -3,12 +3,12 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Homemodel,Loginmodel } from '../viewmodels';
 import Homepage from '../views/Home/Homepage';
-import Loginpage from '../views/Login/Loginpage';
 import Counterdemo from '../views/Counterscreen/counter';
-import Apicall from '../views/Apicall';
 import ThunkApp from '../viewmodels/ReduxApimodel';
 import ApiCallDemo from '../views/ApiCallDemo';
 import fetchData from '../viewmodels/ApiCallModel';
+import Googlemap from '../views/GoogleMap';
+import MapDirections from '../views/GoogleMap/MapDirections';
 
 type rootnode={
     Home:undefined;
@@ -20,13 +20,10 @@ const Stack = createNativeStackNavigator();
 const Routenavigation =() => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Map">
         <Stack.Screen name="Home" component={Homemodel} />
-        <Stack.Screen name="login" component={Loginmodel} />
-        <Stack.Screen name="counter" component={Counterdemo} />
-        {/* <Stack.Screen name="Apicalldemo" component={Apicall} />
-        <Stack.Screen name="reduxApicalldemo" component={ThunkApp} /> */}
-        <Stack.Screen name="calldemo" component={fetchData} />
+        <Stack.Screen name="Map" component={Googlemap} />
+        <Stack.Screen name="Map1" component={MapDirections} />
 
       </Stack.Navigator>
     </NavigationContainer>
---
+++ app/views/GoogleMap/MapDirections.tsx
@@ -0,0 +1,81 @@
+import React,{useState} from 'react';  
+import { StyleSheet, View } from 'react-native';  
+import MapView, { Polyline } from 'react-native-maps';  
+import { Marker } from 'react-native-maps';  
+import MapViewDirections from 'react-native-maps-directions';
+  
+const  MapDirections =()=>{
+    const [coordinates] = useState([
+        {
+            latitude: 28.5995001, longitude: 77.3315623 
+        },
+        {
+            latitude: 28.6001765, longitude: 77.3424825 
+        },
+      ]);
+
+    return (  
+      <View style={styles.MainContainer}>  
+  
+        <MapView  
+          style={styles.mapStyle}  
+          showsUserLocation={true}  
+          zoomEnabled={true}  
+          zoomControlEnabled={true}  
+          initialRegion={{  
+            latitude: 28.5995001,   
+            longitude: 77.3315623,  
+            latitudeDelta: 0.0922,  
+            longitudeDelta: 0.0421,  
+          }}>  
+  
+          <Marker  
+            coordinate={coordinates[0]}  
+            title={"Successive Technologies"}  
+            description={" Successive Technologies"}  
+            pinColor="skyblue"
+            opacity= {1}
+          />  
+        <Marker  
+            coordinate={coordinates[1]}  
+            title={"Shiv mandir"}  
+            opacity= {1}
+          />
+
+          // For This You Must Enable Billing on the Google Cloud Project 
+          <MapViewDirections
+          origin={coordinates[0]}
+          destination={coordinates[1]}
+          apikey={"AIzaSyBrbJ0c-12lV1yF9F4z1yx9myye4rDrxY0"} // insert your API Key here
+          strokeWidth={4}
+          strokeColor="#111111"
+        />
+
+        </MapView>  
+          
+      </View>  
+    );  
+  }  
+  
+
+
+  
+const styles = StyleSheet.create({  
+  MainContainer: {  
+    position: 'absolute',  
+    top: 0,  
+    left: 0,  
+    right: 0,  
+    bottom: 0,  
+    alignItems: 'center',  
+    justifyContent: 'flex-end',  
+  },  
+  mapStyle: {  
+    position: 'absolute',  
+    top: 0,  
+    left: 0,  
+    right: 0,  
+    bottom: 0,  
+  },  
+});  
+export default MapDirections;

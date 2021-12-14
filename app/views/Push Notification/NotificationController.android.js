0a5e00a67245f548beb253f91866ccc304003a11
codePradeep <pradeep.sharma1@successive.tech>
2021-12-14 15:57:24 +0530

    Re Arrenged the Push Noitification

--- app/views/Push Notification/NotificationController.android.js
+++ app/views/Push Notification/NotificationController.android.js
@@ -3,18 +3,18 @@ import { Alert } from 'react-native';
 import messaging from '@react-native-firebase/messaging';
 import PushNotification from 'react-native-push-notification';
 
-// PushNotification.createChannel(
-//   {
-//     channelId: "channel-id", // (required)
-//     channelName: "My channel", // (required)
-//     channelDescription: "A channel to categorise your notifications", // (optional) default: undefined.
-//     playSound: false, // (optional) default: true
-//     soundName: "default", // (optional) See `soundName` parameter of `localNotification` function
-//     importance: 4, // (optional) default: 4. Int value of the Android notification importance
-//     vibrate: true, // (optional) default: true. Creates the default vibration patten if true.
-//   },
-//   (created) => console.log(`createChannel returned '${created}'`) // (optional) callback returns whether the channel was created, false means it already existed.
-// );
+PushNotification.createChannel(
+  {
+    channelId: "channel-id", // (required)
+    channelName: "My channel", // (required)
+    channelDescription: "A channel to categorise your notifications", // (optional) default: undefined.
+    playSound: false, // (optional) default: true
+    soundName: "default", // (optional) See `soundName` parameter of `localNotification` function
+    importance: 4, // (optional) default: 4. Int value of the Android notification importance
+    vibrate: true, // (optional) default: true. Creates the default vibration patten if true.
+  },
+  (created) => console.log(`createChannel returned '${created}'`) // (optional) callback returns whether the channel was created, false means it already existed.
+);
 
 
 
